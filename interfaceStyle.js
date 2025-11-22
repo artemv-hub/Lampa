a8448e2a12de7f2978652bf540bb7791dd2abf49
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-11-22 17:33:52 +0300

    3.2.0

--- interfaceStyle.js
+++ interfaceStyle.js
@@ -3,7 +3,7 @@
 
   let manifest = {
     type: 'interface',
-    version: '3.1.8',
+    version: '3.2.0',
     name: 'UI Style',
     component: 'ui_style'
   };
@@ -44,16 +44,22 @@
   function fixSyncBookmarks() {  
     Lampa.Activity.listener.follow('create', function(e) {  
       if (e.component === 'bookmarks' || e.component === 'favorite') {  
-        const localhost = Lampa.Manifest.plugins.find(p => p.url?.includes('/bookmark.js'))?.url.split('/bookmark.js')[0] || '';  
-
+        var activity = Lampa.Activity.active().activity;  
+        activity.loader(true);  
+  
+        var localhost = Lampa.Manifest.plugins.find(p => p.url?.includes('/bookmark.js'))?.url.split('/bookmark.js')[0] || '';  
+  
         Lampa.Api.request(localhost + '/bookmark/list', '', function(data) {  
           if (data && !data.dbInNotInitialization) {  
             Lampa.Storage.set('favorite', data);  
-            Lampa.Activity.active().activity.render();  
+            activity.render();  
           }  
+          activity.loader(false);  
+        }, function() {  
+          activity.loader(false);  
         });  
       }  
-    });  
+    });
   }
 
   function fixSize() {
