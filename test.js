75b7b25e9f91e6510a02c32dc284d88055c82bd0
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-11-25 20:09:56 +0300

    Update test.js

--- test.js
+++ test.js
@@ -16,83 +16,119 @@
     return minutes + ':' + (secs < 10 ? '0' : '') + secs;  
   }  
   
-  // Функция обновления отображения времени на карточке  
-  function updateCardTimeDisplay(cardElement, hash) {  
+  // Получаем правильный ключ для хранилища  
+  function getViewedKey() {  
     var acc = Lampa.Storage.get('account', '{}');  
-    var viewedKey = 'file_view' + (acc.profile ? '_' + acc.profile.id : '');  
-    var viewed = Lampa.Storage.cache(viewedKey, 10000, {});  
-      
-    var timeData = viewed[hash];  
-      
-    if (timeData && timeData.time && timeData.duration) {  
-      var timeText = formatTime(timeData.time) + ' / ' + formatTime(timeData.duration);  
+    return 'file_view' + (acc.profile ? '_' + acc.profile.id : '');  
+  }  
+  
+  // Обновление отображения времени  
+  function updateCardTimeDisplay(cardElement, hash) {  
+    try {  
+      var viewed = Lampa.Storage.cache(getViewedKey(), 10000, {});  
+      var timeData = viewed[hash];  
         
-      // Находим элемент с процентами и заменяем текст  
-      var percentElement = cardElement.find('.card__view-percent');  
-      if (percentElement.length) {  
-        percentElement.text(timeText);  
+      if (timeData && timeData.time && timeData.duration) {  
+        var timeText = formatTime(timeData.time) + ' / ' + formatTime(timeData.duration);  
+          
+        // Пробуем разные селекторы для поиска элемента  
+        var selectors = [  
+          '.card__view-percent',  
+          '.card-watched__line',  
+          '.card__time',  
+          '.card-watched__text',  
+          '.card__progress-text'  
+        ];  
+          
+        var updated = false;  
+        for (var i = 0; i < selectors.length; i++) {  
+          var element = cardElement.find(selectors[i]);  
+          if (element.length) {  
+            element.text(timeText);  
+            updated = true;  
+            break;  
+          }  
+        }  
+          
+        if (!updated) {  
+          console.log('TimeFormat: элемент для отображения не найден', hash);  
+        }  
       }  
-        
-      // Также обновляем линию прогресса, если есть  
-      var lineElement = cardElement.find('.card-watched__line');  
-      if (lineElement.length) {  
-        lineElement.attr('data-time', timeText);  
-      }  
+    } catch (e) {  
+      console.error('TimeFormat error:', e);  
     }  
   }  
   
-  // Перехватываем создание карточек  
-  function patchCardCreation() {  
-    Lampa.Listener.follow('card', function(e) {  
-      if (e.object && e.object.card) {  
-        var card = e.object.card;  
-        var hash = card.hash || (card.id + '_' + (card.name ? 'tv' : 'movie'));  
+  // Перехватываем рендеринг карточек  
+  function patchCardRender() {  
+    // Перехватываем метод render карточек  
+    if (Lampa.CardView && Lampa.CardView.prototype.render) {  
+      var originalRender = Lampa.CardView.prototype.render;  
+      Lampa.CardView.prototype.render = function() {  
+        var result = originalRender.apply(this, arguments);  
           
-        // Обновляем сразу при создании карточки  
-        updateCardTimeDisplay($(card), hash);  
-      }  
-    });  
+        if (this.card && this.card.id) {  
+          var hash = this.card.id + '_' + (this.card.name ? 'tv' : 'movie');  
+          updateCardTimeDisplay($(this.render()), hash);  
+        }  
+          
+        return result;  
+      };  
+    }  
   }  
   
-  // Перехватываем обновление данных таймкодов  
-  function patchTimecodeUpdate() {  
+  // Слушаем события Lampa  
+  function setupListeners() {  
+    // При обновлении таймкодов  
     Lampa.Listener.follow('lampac', function(e) {  
       if (e.type == 'timecode_pullFromServer') {  
-        // После обновления таймкодов с сервера, обновляем все видимые карточки  
-        setTimeout(function() {  
-          $('.card').each(function() {  
-            var cardElement = $(this);  
-            var cardData = cardElement.data('card');  
-            if (cardData) {  
-              var hash = cardData.hash || (cardData.id + '_' + (cardData.name ? 'tv' : 'movie'));  
-              updateCardTimeDisplay(cardElement, hash);  
-            }  
-          });  
-        }, 100);  
+        console.log('TimeFormat: обновление после синхронизации');  
+        setTimeout(updateAllCards, 200);  
+      }  
+    });  
+      
+    // При изменении активности  
+    Lampa.Listener.follow('activity', function(e) {  
+      if (e.type == 'start') {  
+        console.log('TimeFormat: обновление при смене активности');  
+        setTimeout(updateAllCards, 500);  
       }  
     });  
   }  
   
-  // Периодическое обновление всех карточек  
+  // Обновить все видимые карточки  
+  function updateAllCards() {  
+    $('.card').each(function() {  
+      var cardElement = $(this);  
+      var cardData = cardElement.data('card');  
+        
+      if (cardData && cardData.id) {  
+        var hash = cardData.id + '_' + (cardData.name ? 'tv' : 'movie');  
+        updateCardTimeDisplay(cardElement, hash);  
+      }  
+    });  
+  }  
+  
+  // Периодическое обновление  
   function startPeriodicUpdate() {  
-    setInterval(function() {  
-      $('.card').each(function() {  
-        var cardElement = $(this);  
-        var cardData = cardElement.data('card');  
-        if (cardData) {  
-          var hash = cardData.hash || (cardData.id + '_' + (cardData.name ? 'tv' : 'movie'));  
-          updateCardTimeDisplay(cardElement, hash);  
-        }  
-      });  
-    }, 5000); // Обновляем каждые 5 секунд  
+    setInterval(updateAllCards, 10000); // Каждые 10 секунд  
   }  
   
   function startPlugin() {  
+    if (window.lampac_timeformat_plugin) return;  
     window.lampac_timeformat_plugin = true;  
       
-    patchCardCreation();  
-    patchTimecodeUpdate();  
+    console.log('TimeFormat plugin starting...');  
+      
+    patchCardRender();  
+    setupListeners();  
     startPeriodicUpdate();  
+      
+    // Первое обновление через 2 секунды после загрузки  
+    setTimeout(function() {  
+      console.log('TimeFormat: первое обновление');  
+      updateAllCards();  
+    }, 2000);  
       
     console.log('TimeFormat plugin loaded - always visible mode');  
   }  
