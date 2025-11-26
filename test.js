ccb540f1a56dce5bd992ba3744f7462a9b83031b
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-11-26 12:21:33 +0300

    secondsToTimeHuman

--- test.js
+++ test.js
@@ -1,32 +1,26 @@
-(function() {  
-    'use strict';  
-      
-    function init() {  
-        if (!window.Lampa || !Lampa.Utils) {  
-            setTimeout(init, 500);  
-            return;  
-        }  
-          
-        // Переопределяем secondsToTimeHuman для короткого формата  
-        const originalSecondsToTimeHuman = Lampa.Utils.secondsToTimeHuman;  
-          
-        Lampa.Utils.secondsToTimeHuman = function(seconds) {  
-            // Для фильмов возвращаем короткий формат  
-            if (typeof seconds === 'number' && seconds > 0) {  
-                const hours = Math.floor(seconds / 3600);  
-                const minutes = Math.floor((seconds % 3600) / 60);  
-                const secs = Math.floor(seconds % 60);  
-                  
-                return hours > 0   
-                    ? ${hours}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}  
-                    : ${minutes}:${secs.toString().padStart(2, '0')};  
-            }  
-              
-            return originalSecondsToTimeHuman.apply(this, arguments);  
-        };  
-          
-        console.log('[Fix Time Format] Plugin loaded');  
-    }  
-      
-    init();  
+(function () {
+  'use strict';
+
+  function init() {
+    const originalSecondsToTimeHuman = Lampa.Utils.secondsToTimeHuman;
+
+    Lampa.Utils.secondsToTimeHuman = function (seconds) {
+      // Для фильмов возвращаем короткий формат  
+      if (typeof seconds === 'number' && seconds > 0) {
+        const hours = Math.floor(seconds / 3600);
+        const minutes = Math.floor((seconds % 3600) / 60);
+        const secs = Math.floor(seconds % 60);
+
+        return hours > 0
+          ? `${hours}:${minutes.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`
+          : `${minutes}:${secs.toString().padStart(2, '0')}`;
+      };
+
+      return originalSecondsToTimeHuman.apply(this, arguments);
+    };
+
+    console.log('[Fix Time Format] Plugin loaded');
+  }
+
+  init();
 })();
