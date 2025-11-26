00d8acb13eaf90e59d36508ac7c0374d8e2cb03e
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-11-26 10:53:18 +0300

    secondsToTimeHuman

--- test.js
+++ test.js
@@ -11,7 +11,6 @@
         const originalSecondsToTimeHuman = Lampa.Utils.secondsToTimeHuman;  
           
         Lampa.Utils.secondsToTimeHuman = function(seconds) {  
-            // Для фильмов возвращаем короткий формат  
             if (typeof seconds === 'number' && seconds > 0) {  
                 const hours = Math.floor(seconds / 3600);  
                 const minutes = Math.floor((seconds % 3600) / 60);  
@@ -24,19 +23,113 @@
               
             return originalSecondsToTimeHuman.apply(this, arguments);  
         };  
-          const originalView = Lampa.Timeline.view;  
-  
-Lampa.Timeline.view = function(hash) {  
-    const result = originalView.call(this, hash);  
-      
-    // Для фильмов всегда устанавливаем time, чтобы не было процентов  
-    if (result && result.percent && !result.time) {  
-        result.time = Math.floor(result.percent * 90); // пример: 50% = 45 секунд для 90-секундного видео  
-    }  
-      
-    return result;  
-};
-        console.log('[Fix Time Format] Plugin loaded');  
+          
+        // Переопределяем watched для фильмов - убираем "Просмотрено" и добавляем полное время  
+        if (Lampa.Card && Lampa.Card.module && Lampa.Card.module.watched) {  
+            const originalOnWatched = Lampa.Card.module.watched.onWatched;  
+              
+            Lampa.Card.module.watched.onWatched = function() {  
+                if (!Lampa.Storage.field('card_episodes')) return;  
+                  
+                if (!this.watched_checked) {  
+                    let data = this.data;  
+                      
+                    function get(callback) {  
+                        if (data.original_name) Lampa.Timetable.get(data, callback);  
+                        else callback([]);  
+                    }  
+                      
+                    get((episodes, from_db) => {  
+                        let viewed;  
+                          
+                        let Draw = () => {  
+                            episodes.forEach(ep => {  
+                                let hash = Lampa.Utils.hash([ep.season_number, ep.season_number > 10 ? ':' : '', ep.episode_number, data.original_title].join(''));  
+                                let view = Lampa.Timeline.view(hash);  
+                                  
+                                if (view.percent) viewed = {ep, view};  
+                            });  
+                              
+                            if (!viewed && data.original_name) {  
+                                let last = Lampa.Storage.get('online_watched_last', '{}');  
+                                let filed = last[Lampa.Utils.hash(data.original_title)];  
+                                  
+                                if (filed && filed.episode) {  
+                                    viewed = {  
+                                        ep: {  
+                                            episode_number: filed.episode,  
+                                            name: Lampa.Lang.translate('full_episode') + ' ' + filed.episode,  
+                                        },  
+                                        view: Lampa.Timeline.view(Lampa.Utils.hash([filed.season, filed.season > 10 ? ':' : '',filed.episode,data.original_title].join('')))  
+                                    };  
+                                }  
+                            }  
+                              
+                            // Главное изменение для фильмов  
+                            if (!viewed && !data.original_name) {  
+                                let time = Lampa.Timeline.watched(data, true);  
+                                  
+                                if (time.percent) {  
+                                    viewed = {  
+                                        ep: {  
+                                            // Убрали Lang.translate('title_viewed') и добавили полный формат  
+                                            name: Lampa.Utils.secondsToTime(time.time, true) + '/' + Lampa.Utils.secondsToTime(time.duration, true),  
+                                        },  
+                                        view: time  
+                                    };  
+                                }  
+                            }  
+                              
+                            if (!viewed && data.original_name) {  
+                                let any = Lampa.Timeline.watched(data, true).pop();  
+                                  
+                                if (any) viewed = {ep: {  
+                                    name: Lampa.Lang.translate('full_episode') + ' ' + any.ep,  
+                                }, view: any.view};  
+                            }  
+                              
+                            if (viewed) {  
+                                let wrap = Lampa.Template.js('card_watched', {});  
+                                  
+                                let div = document.createElement('div');  
+                                let span = document.createElement('span');  
+                                  
+                                div.addClass('card-watched__item');  
+                                div.append(span);  
+                                  
+                                span.innerText = viewed.ep.name;  
+                                  
+                                if (viewed.view) {  
+                                    let timeline = Lampa.Timeline.render(viewed.view)[0];  
+                                    div.append(timeline);  
+                                }  
+                                  
+                                wrap.find('.card-watched__body').append(div);  
+                                  
+                                this.watched_wrap = wrap;  
+                                let view = this.html.find('.card__view');  
+                                view.insertBefore(wrap, view.firstChild);  
+                            }  
+                        };  
+                          
+                        Draw();  
+                    });  
+                      
+                    this.watched_checked = true;  
+                }  
+            };  
+        }  
+          
+        // Для постоянного отображения добавляем CSS  
+        const style = document.createElement('style');  
+        style.textContent = `  
+            .card-watched {  
+                display: block !important;  
+            }  
+        `;  
+        document.head.appendChild(style);  
+          
+        console.log('[Always Time Format] Plugin loaded');  
     }  
       
     init();  
