226302f570d7254ba0e977a8708a350b47d668b9
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-11-27 15:59:52 +0300

    3.4.4

--- interfaceFix.js
+++ interfaceFix.js
@@ -3,7 +3,7 @@
 
   let manifest = {
     type: 'interface',
-    version: '3.4.3',
+    version: '3.4.4',
     name: 'UI Fix',
     component: 'ui_fix'
   }
@@ -27,19 +27,21 @@
   }
 
   function fixSyncBookmarks() {
-    Lampa.WebSocket.on('bookmark', function (event) {
-      if (event.type === 'set' || event.type === 'add' || event.type === 'remove') {
-        Lampa.Api.request('{localhost}/bookmark/list', '', function (data) {
-          if (data && !data.dbInNotInitialization) {
-            Lampa.Storage.set('favorite', data);
-            const active = Lampa.Activity.active();
-            if (active && (active.component === 'bookmarks' || active.component === 'favorite')) {
-              active.activity.render();
+    if (typeof Lampa.WebSocket !== 'undefined') {
+      Lampa.WebSocket.on('bookmark', function (event) {
+        if (event.type === 'set' || event.type === 'add' || event.type === 'remove') {
+          Lampa.Api.request('{localhost}/bookmark/list', '', function (data) {
+            if (data && !data.dbInNotInitialization) {
+              Lampa.Storage.set('favorite', data);
+              const active = Lampa.Activity.active();
+              if (active && (active.component === 'bookmarks' || active.component === 'favorite')) {
+                active.activity.render();
+              }
             }
-          }
-        });
-      }
-    });
+          });
+        }
+      });
+    }
   }
 
   function fixSize() {
