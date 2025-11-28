13b69ce61e8d04e7d426e72d2e4394463afbe858
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-11-28 21:41:40 +0300

    3.5.3

--- interfaceFix.js
+++ interfaceFix.js
@@ -3,7 +3,7 @@
 
   let manifest = {
     type: 'interface',
-    version: '3.5.2'',
+    version: '3.5.3'',
     name: 'UI Fix',
     component: 'ui_fix'
   };
@@ -109,7 +109,7 @@
   }
 
   function startPlugin() {
-    fixSyncBookmarks();
+    //fixSyncBookmarks();
     fixLabelsTV();
     fixButtons();
     fixTitle();
