6fadc81357189ad07ac206f1475b2cb734bc4ad4
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-11-30 21:03:02 +0300

    3.5.44

--- interfaceFix.js
+++ interfaceFix.js
@@ -3,7 +3,7 @@
 
   let manifest = {
     type: 'interface',
-    version: '3.5.43',
+    version: '3.5.44',
     name: 'UI Fix',
     component: 'ui_fix'
   };
@@ -64,8 +64,6 @@
       this.limit_view = 12;  
     };  
 
-    Lampa.Storage.listener.remove('change', Lampa.Layer.size);
-    // Переопределяем size()  
     Lampa.Layer.size = function() {  
         let selectedLevel = Lampa.Storage.field('interface_size');  
         let sizeMap = {  
@@ -74,11 +72,8 @@
             bigger: 14  
         };  
         let fontSize = Lampa.Platform.screen('mobile') ? 10 : sizeMap[selectedLevel];  
-        $('body').css({ fontSize: fontSize + 'px' })  
-                  .removeClass('size--small size--normal size--bigger').addClass('size--' + selectedLevel);  
+        $('body').css({ fontSize: fontSize + 'px' });
     };  
-      
-    // Вызываем только один раз  
     Lampa.Layer.size();  
   }
 
