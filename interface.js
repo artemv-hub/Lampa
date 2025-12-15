443f03e0532e9ad3b9670c5fb9a0a5ee91e58ace
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-12-15 10:01:54 +0300

    3.7.13

--- interface.js
+++ interface.js
@@ -3,7 +3,7 @@
 
   let manifest = {
     type: 'interface',
-    version: '3.7.12',
+    version: '3.7.13',
     name: 'UI Style',
     component: 'ui_style'
   };
@@ -16,11 +16,19 @@
     .full-start-new__buttons .full-start__button:not(.focus) span { display: unset; }
     .full-start__title-original { font-size: 1.6em; margin-bottom: 0em; }
 
-    .card__watched { font-weight: bold; font-size: 1.2em; left: 50%; bottom: -0.8em; border-radius: 1em; padding: 0.2em 0.4em; background: rgb(29, 31, 32); position: absolute; transform: translate(-50%); white-space: nowrap; }
-    .card__quality { font-weight: bold; font-size: 1.2em; left: -0.4em; top: 2.4em; bottom: unset; border-radius: 0.2em; padding: 0.2em 0.2em; color: #000; text-transform: none; }
-    .card__vote { font-weight: bold; font-size: 1.2em; left: -0.4em; right: unset; top: 0.8em; bottom: unset; border-radius: 0.2em; padding: 0.2em 0.2em; color: #000; }
-    .card__type { font-size: 1em; left: 50%; top: -1em; border-radius: 1em; padding: 0.2em 0.4em; color: #fff; background: rgb(29, 31, 32); transform: translate(-50%);}
-    .card__marker { display: none; }
+    .card__watched        { position: absolute; left: 50%; bottom: -0.8em; transform: translate(-50%);
+                            font-size: 1.2em; font-weight: bold; white-space: nowrap;
+                            border-radius: 1em; padding: 0.2em 0.4em; background: rgb(29, 31, 32); }
+    .card__quality        { left: -0.4em; top: 2.4em; bottom: unset;
+                            font-size: 1.2em; font-weight: bold; text-transform: none;
+                            border-radius: 0.2em; padding: 0.2em 0.2em; color: #000; }
+    .card__vote           { left: -0.4em; right: unset; top: 0.8em; bottom: unset;
+                            font-size: 1.2em; font-weight: bold;
+                            border-radius: 0.2em; padding: 0.2em 0.2em; color: #000; }
+    .card--tv .card__type { left: 50%; top: -0.8em; transform: translate(-50%);
+                            font-size: 1em;
+                            border-radius: 1em; padding: 0.2em 0.4em; color: #fff; background: rgb(29, 31, 32); }
+    .card__marker         { display: none; }
 
     .time-line > div,
     .torrent-serial__progress,
