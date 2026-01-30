7ad73bbde56ef27a924a9ef1cb5efce342f7afd1
Артём <82865318+artemv-hub@users.noreply.github.com>
2026-01-30 19:38:27 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 (function(){  
+    const originalSelectShow = Lampa.Select.show;  
+      
     Lampa.Select.show = function(params){  
         if(params.title === Lampa.Lang.translate('title_out')){  
             const confirm = params.items.find(i=>i.out);  
             if(confirm && params.onSelect) params.onSelect(confirm);  
             return;  
         }  
-        return Lampa.Select.show.original.call(this, params);  
+        return originalSelectShow.call(this, params);  
     };  
-    Lampa.Select.show.original = Lampa.Select.show;  
 })();
