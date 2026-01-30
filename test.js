5e1527adf7c76281bb70991069a8657358927bb6
Артём <82865318+artemv-hub@users.noreply.github.com>
2026-01-30 19:04:15 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,39 @@
-// Переопределяем обработчик кнопки "назад" для прямого выхода  
-        Lampa.Listener.follow('backward', (event) => {  
-            let noout = Lampa.Platform.is('browser') || Lampa.Platform.desktop();  
+(function () {  
+  "use strict";  
   
-            if (event.count == 1 && Date.now() > Lampa.Activity.start_time + (1000 * 2) && !noout) {  
-                let enabled = Lampa.Controller.enabled().name;  
-                  
-                // Прямой выход без меню подтверждения  
-                Lampa.Activity.out();  
-                Lampa.Controller.toggle(enabled);  
-                Lampa.App.close();  
-            }  
-        });  
+  let manifest = {  
+    type: 'other',  
+    version: '1.0.0',  
+    name: 'Direct Exit',  
+    component: 'direct_exit'  
+  };  
+  
+  Lampa.Manifest.plugins = manifest;  
+  
+  function init() {  
+    // Переопределяем обработчик кнопки "назад" для прямого выхода  
+    Lampa.Listener.follow('backward', (event) => {  
+      let noout = Lampa.Platform.is('browser') || Lampa.Platform.desktop();  
+  
+      if (event.count == 1 && Date.now() > Lampa.Activity.start_time + (1000 * 2) && !noout) {  
+        let enabled = Lampa.Controller.enabled().name;  
+          
+        // Прямой выход без меню подтверждения  
+        Lampa.Activity.out();  
+        Lampa.Controller.toggle(enabled);  
+        Lampa.App.close();  
+      }  
+    });  
+  }  
+  
+  if (window.appready) {   
+    init();   
+  }  
+  else {  
+    Lampa.Listener.follow("app", function (e) {  
+      if (e.type === "ready") {   
+        init();   
+      }  
+    });  
+  }  
+})();
