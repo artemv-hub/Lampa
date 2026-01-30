d8b0996280f1f2de97a9a4db992c4758e7ff7c2f
Артём <82865318+artemv-hub@users.noreply.github.com>
2026-01-30 18:53:53 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
-listener.follow('backward',(event)=>{  
-    let noout = Platform.is('browser') || Platform.desktop();  
+// Переопределяем обработчик кнопки "назад" для прямого выхода  
+        Lampa.Listener.follow('backward', (event) => {  
+            let noout = Lampa.Platform.is('browser') || Lampa.Platform.desktop();  
   
-    if(event.count == 1 && Date.now() > start_time + (1000 * 2) && !noout){  
-        let enabled = Controller.enabled().name;  
-          
-        out();  
-        Controller.toggle(enabled);  
-        App.close();  
-    }  
-});
+            if (event.count == 1 && Date.now() > Lampa.Activity.start_time + (1000 * 2) && !noout) {  
+                let enabled = Lampa.Controller.enabled().name;  
+                  
+                // Прямой выход без меню подтверждения  
+                Lampa.Activity.out();  
+                Lampa.Controller.toggle(enabled);  
+                Lampa.App.close();  
+            }  
+        });  
