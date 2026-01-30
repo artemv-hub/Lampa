07a3ae3ac56c0434a6e4ca30b600cb9037faba34
Артём <82865318+artemv-hub@users.noreply.github.com>
2026-01-30 19:53:20 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,13 @@
     Lampa.Select.show = function(params){  
         if(params.title === Lampa.Lang.translate('title_out')){  
             const confirm = params.items.find(i=>i.out);  
-            if(confirm && params.onSelect) params.onSelect(confirm);  
-            return;  
+            if(confirm && params.onSelect) {  
+                // Выполняем полный выход как в оригинале  
+                Lampa.Activity.out();  
+                Lampa.Controller.toggle(Lampa.Controller.enabled().name);  
+                Lampa.App.close();  
+                return;  
+            }  
         }  
         return Lampa.Select.show.original.call(this, params);  
     };  
